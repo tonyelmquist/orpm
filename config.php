b2a4d98bb4278b81b1440bf02cb160c72f67ceef
Lawrence Nderitu Keru <ndech90@gmail.com>
2016-06-17 10:16:37 +0300

    Installation
    DB Setup

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
+	$dbPassword = '';
+	$dbDatabase = 'orpm';
+
+	$adminConfig = array(
+		'adminUsername' => "orpm_admin",
+		'adminPassword' => "7a94abf5308fd29fc811bede50783e16",
+		'notifyAdminNewMembers' => "",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "ndech90@gmail.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://localhost:8080/orpm\n\nRegards,\nAdmin",
+		'hide_twitter_feed' => ""
+	);
