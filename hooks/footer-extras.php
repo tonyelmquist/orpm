72356027459bb83212bbdcf31a4b3ab6bfea8181
Lawrence Nderitu Keru <ndech90@gmail.com>
2016-06-17 14:58:36 +0300

    Background Image
    Fixed background image, Custom footer

---
+++ hooks/footer-extras.php
@@ -0,0 +1,40 @@
+<?php
+
+	$script_name = basename($_SERVER['PHP_SELF']);
+	if($script_name == 'index.php' && isset($_GET['signIn'])){
+		?>
+		 <style>
+			body{
+				background: url("images/MenuMDIImageold.jpg") no-repeat fixed center center / cover;
+				
+			}
+		</style>
+	<!--	<div class="alert alert-primary" id="benefits">
+			Benefits of becoming a member:
+			<ul>
+				<li> You'll be awesome!
+				<li> We'll be richer!
+			</ul>
+		</div> 
+		
+		<script>
+			$j(function(){
+				$j('#benefits').appendTo('#login_splash');
+			})
+		</script>   -->
+		<?php
+	}
+	if($script_name == 'membership_signup.php'){
+		?>
+			 <style>
+				body{
+					background: url("images/MenuMDIImageold.jpg") no-repeat fixed center center / cover;					
+				}
+			</style>
+			<?php
+		}
+?>
+
+<div class="navbar-fixed-bottom hidden-print alert alert-info">
+	<?php echo date('D, j M Y h:m:s a T'); ?>
+</div>
