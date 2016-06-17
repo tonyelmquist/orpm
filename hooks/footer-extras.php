69076b97b55d1d1832d122c92e0f7f1f5a1697fa
Lawrence Nderitu Keru <ndech90@gmail.com>
2016-06-17 15:42:17 +0300

    LoginFailed
    Fixed Login Failed

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -24,6 +24,29 @@
 		</script>   -->
 		<?php
 	}
+	if($script_name == 'index.php' && isset($_GET['loginFailed'])){
+		?>
+			 <style>
+				body{
+					background: url("images/MenuMDIImageold.jpg") no-repeat fixed center center / cover;
+					
+				}
+			</style>
+		<!--	<div class="alert alert-primary" id="benefits">
+				Benefits of becoming a member:
+				<ul>
+					<li> You'll be awesome!
+					<li> We'll be richer!
+				</ul>
+			</div> 
+			
+			<script>
+				$j(function(){
+					$j('#benefits').appendTo('#login_splash');
+				})
+			</script>   -->
+			<?php
+		}
 	if($script_name == 'membership_signup.php'){
 		?>
 			 <style>
